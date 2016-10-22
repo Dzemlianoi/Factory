27130259ed1206b36f5dbffdb62037ef9439b867
dzemlianoi <dzemlianoi@gmail.com>
2016-10-22 16:24:22 +0300

    factory done

---
+++ factory.rb
@@ -0,0 +1,23 @@
+class Factory
+  def self.new(*attrs, &block)
+    Class.new do
+      attr_accessor(*attrs)
+
+      define_method :attributes do
+        attrs
+      end
+
+      def initialize(*values)
+        raise 'Argument Error' if values.count != attributes.count
+        ziped = attributes.zip(values)
+        ziped.each do |k, v|
+          instance_variable_set("@#{k}", v)
+        end
+      end
+      class_eval(&block) if block_given?
+    end
+  end
+end
+
+Job = Factory.new(:spesialization, :salary, :work_hours, :cookies)
+programmer = Job.new('developer', 1, 9, true)
